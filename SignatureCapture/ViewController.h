3769fca0660a08c38eb461078e4141e7d706ab0f
Jude Jonassaint <jude@sicklesoft.com>
2015-10-31 19:06:41 -0400

    Initial Commit

---
+++ SignatureCapture/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SignatureCapture
+//
+//  Created by Jude Jonassaint on 10/31/15.
+//  Copyright © 2015 Sicklesoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SignatureCapture/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SignatureCapture
+//
+//  Created by Jude Jonassaint on 10/31/15.
+//  Copyright © 2015 Sicklesoft. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
